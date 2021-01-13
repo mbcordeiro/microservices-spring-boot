2162c740e7d2c8a9b749a528927f52f87b214926
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-01-13 00:16:34 -0300

    First commit

---
+++ hr-worker/src/test/java/com/matheuscordeiro/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
